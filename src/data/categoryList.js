3d4a6205475efb7009770f3d1a57041a3fd39e99
Jelby <evgeniy.ipatov92@mail.ru>
2022-06-10 13:58:08 +0300

    docs: add tasks for implementation by main block on home page

---
+++ src/data/categoryList.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    category_id: 1,
+    name: "Название категории",
+    description: "Описание категории",
+    children: [],
+  },
+];
---
+++ src/data/productList.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    product_id: 1,
+    name: "Название товара",
+    sku: "Артикул товара",
+    description: "Описание товара",
+    price: 100,
+    image: "Картинка товара",
+    category_id: 0,
+  },
+];
