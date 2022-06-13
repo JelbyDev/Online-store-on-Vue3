d1ae10495367fd33c4adc2cef6c851f53ab14229
Jelby <evgeniy.ipatov92@mail.ru>
2022-06-13 17:49:21 +0300

    feat: add design in good products block

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <home-banner></home-banner>
     <goods-products class="home-page__best-products"></goods-products>
     <section class="home-page__seo">
-      <div class="content-wrapper">
+      <div class="app-content-wrapper">
         <h1>SEO text</h1>
         <p>SEO текст тестовая часть</p>
       </div>
@@ -19,7 +19,7 @@ import AppMain from "@/components/AppMain";
 import AppFooter from "@/components/AppFooter.vue";
 
 import HomeBanner from "@/components/HomeBanner.vue";
-import GoodsProducts from "@/components/HomeGoodsProducts.vue";
+import GoodsProducts from "@/components/GoodsProducts.vue";
 
 export default {
   components: {
@@ -39,6 +39,7 @@ export default {
 html,
 body {
   height: 100%;
+  font-size: 16px;
 }
 .app-layout {
   min-height: 100%;
@@ -46,9 +47,41 @@ body {
   flex-direction: column;
   justify-content: space-between;
 }
-.content-wrapper {
+
+.app-content-wrapper {
   width: 1280px;
   margin: 0 auto;
   padding: 0 13px;
+}
+.app-h2-title {
+  font-weight: bold;
+  text-align: center;
+  font-size: 2rem;
+  margin: 20px 0;
+}
+.app-old-price {
+  color: #ccc;
+  text-decoration: line-through;
+}
+.app-btn {
+  display: block;
+  cursor: pointer;
+  transition: background-color, color 0.3s;
+  padding: 10px 15px;
+  font-size: 1rem;
+  border: 1px solid transparent;
+  border-radius: 7px;
+}
+.app-btn-orange {
+  background-color: #ff9a56;
+  color: #fff;
+}
+.app-btn-orange:hover {
+  background-color: #ffc198;
+  color: #000;
+}
+
+.app-fl-4 {
+  width: 25%;
 }
 </style>
